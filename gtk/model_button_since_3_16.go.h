6d3b70e412989692873fce3cb51feb658cf390d2
MJacred <loesch.benny92@gmx.de>
2019-09-15 16:01:34 +0200

    created bindings for GtkModelButton

--- gtk/model_button_since_3_16.go.h
+++
@@ -1,7 +0,0 @@
-// +build !gtk_3_6,!gtk_3_8,!gtk_3_10,!gtk_3_12,!gtk_3_16
-
-static GtkModelButton *
-toGtkModelButton(void *mb)
-{
-	return (GTK_MODEL_BUTTON(mb));
-}
--- gtk/popover_menu_sine_3_16.go.h
+++
@@ -1,7 +0,0 @@
-// +build !gtk_3_6,!gtk_3_8,!gtk_3_10,!gtk_3_12,!gtk_3_16
-
-static GtkPopoverMenu *
-toGtkPopoverMenu(void *p)
-{
-	return (GTK_POPOVER_MENU(p));
-}

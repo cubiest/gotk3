e0426ea75e9a74e41e24c3323fcb13caf4057d30
MJacred <loesch.benny92@gmx.de>
2020-02-20 16:49:47 +0100

    adding Gdk Button constants

--- gdk/gdk.go
+++ gdk/gdk.go
@@ -374,6 +374,15 @@ const (
 	GDK_EVENT_STOP      bool = C.GDK_EVENT_STOP != 0
 )
 
+// Button constants
+type Button uint
+
+const (
+	BUTTON_PRIMARY   Button = C.GDK_BUTTON_PRIMARY
+	BUTTON_MIDDLE    Button = C.GDK_BUTTON_MIDDLE
+	BUTTON_SECONDARY Button = C.GDK_BUTTON_SECONDARY
+)
+
 // CrossingMode is a representation of GDK's GdkCrossingMode.
 
 type CrossingMode int

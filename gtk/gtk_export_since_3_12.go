a150e13cf3bb77afae89ee89f1ff4fa7ce10d343
MJacred <loesch.benny92@gmx.de>
2025-01-21 20:12:32 +0100

    Add sort func bindings for GtkFlowBox

---
+++ gtk/gtk_export_since_3_12.go
@@ -0,0 +1,22 @@
+// Same copyright and license as the rest of the files in this project
+//go:build !gtk_3_6 && !gtk_3_8 && !gtk_3_10
+// +build !gtk_3_6,!gtk_3_8,!gtk_3_10
+
+package gtk
+
+import "C"
+import (
+	"unsafe"
+
+	"github.com/gotk3/gotk3/glib"
+	"github.com/gotk3/gotk3/internal/callback"
+)
+
+//export goFlowBoxSortFuncs
+func goFlowBoxSortFuncs(child1 *C.GtkFlowBoxChild, child2 *C.GtkFlowBoxChild, userData C.gpointer) C.gint {
+	fn := callback.Get(uintptr(userData)).(FlowBoxSortFunc)
+	return C.gint(fn(
+		wrapFlowBoxChild(glib.Take(unsafe.Pointer(child1))),
+		wrapFlowBoxChild(glib.Take(unsafe.Pointer(child2))),
+	))
+}
